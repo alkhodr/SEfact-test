e5ccddf45faad0787d90611ef530bfe9bb19fe2f
Alkhodr Hallal <alkhodrhallal@gmail.com>
2023-08-17 04:08:45 +0300

    First Question Done

---
+++ Flipper.py
@@ -0,0 +1,10 @@
+#First Question in the Assigment
+def reverse_and_concatenate(s, i):
+    reversed_s = s[::-1]  # Reverse the string using slicing
+    result = reversed_s * i  # Concatenate the reversed string i times
+    return result
+#______________fiLL HEEEErrrrEEEE___________
+print(reverse_and_concatenate("hello", 2))  #<<<<<<<<
+#----------------------------------------------------------------
+#Second Question
+#----------------------------------------------------------------
